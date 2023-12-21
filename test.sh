07e806a388738fba97edca3d8a41b32ad9d00819
Nikesh <subedi.nikesh@outlook.com>
2023-12-21 21:18:57 +0545

    removed hello world container

--- test.sh
+++ test.sh
@@ -18,6 +18,3 @@ echo \
 sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-
-# Running a test container
-sudo docker run hello-world
